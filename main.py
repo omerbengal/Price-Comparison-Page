d68f3e134044f1effef56b1479972a68b1b65dd6
Omer Bengal <omer.bengal01@post.runi.ac.il>
2024-04-24 11:58:33 +0300

    Structure ready for branching

--- main.py
+++ main.py
@@ -1,17 +1,47 @@
 import requests
-import beautifulsoup4 as bs4
+from bs4 import BeautifulSoup
+
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 \
+                (Windows NT 10.0; Win64; x64) \
+                AppleWebKit/537.36 \
+                (KHTML, like Gecko) \
+                Chrome/123.0.0.0 Safari/537.36'
+}
+
+
+def get_response(url: str) -> requests.models.Response:
+    return requests.get(url, headers=HEADERS)
 
 
 def get_best_buy_price(item: str) -> dict:
-    pass
+    url = ''
+    response = get_response(url)
+    # check if response is valid
+    if response.status_code == 200:
+        return {}
+    else:
+        return {'error': 'Invalid response'}
 
 
 def get_walmart_price(item: str) -> dict:
-    pass
+    url = ''
+    response = get_response(url)
+    # check if response is valid
+    if response.status_code == 200:
+        return {}
+    else:
+        return {'error': 'Invalid response'}
 
 
 def get_newegg_price(item: str) -> dict:
-    pass
+    url = ''
+    response = get_response(url)
+    # check if response is valid
+    if response.status_code == 200:
+        return {}
+    else:
+        return {'error': 'Invalid response'}
 
 
 # Main
