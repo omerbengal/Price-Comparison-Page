236310aa1302f9bafa171bc7842a53637436459d
Omer Bengal <omer.bengal01@post.runi.ac.il>
2024-04-24 11:40:26 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests
+import beautifulsoup4 as bs4
+
+
+def get_best_buy_price(item: str) -> dict:
+    pass
+
+
+def get_walmart_price(item: str) -> dict:
+    pass
+
+
+def get_newegg_price(item: str) -> dict:
+    pass
+
+
+# Main
+if __name__ == "__main__":
+    pass
