c537c14108eaedee5fbf64b14c757e2ea8b433e8
Omer Bengal <omer.bengal01@post.runi.ac.il>
2024-04-29 11:28:36 +0300

    Structure fixes

--- main.py
+++ main.py
@@ -34,6 +34,10 @@ def get_best_buy_price(item: str) -> dict:
 
         # Get item page link: inside the h4 tag there is a <a> tag with the link
         item_page_link = item.find('h4', class_='sku-title').a['href']
+
+        if not item_page_link:
+            raise Exception("Item page link not found in first item")
+
         full_item_page_link = f'https://www.bestbuy.com{item_page_link}&intl=nosplash'  # nopep8
 
         # Get item page response
@@ -113,20 +117,19 @@ def get_newegg_price(item: str) -> dict:
         # Find the first div with class "item-container"
         first_item_container_div = soup.find('div', class_='item-container')
 
-        # Check if the div was found
-        if first_item_container_div:
-            # Find the first <a> element inside the div
-            first_a_inside_div = first_item_container_div.find('a')
+        # Check if the div was not found
+        if not first_item_container_div:
+            raise Exception("No items found in Newegg search page")
 
-            # Check if the <a> element was found
-            if first_a_inside_div:
-                # Extract the href attribute
-                final_url = first_a_inside_div.get('href')
-            else:
-                print(
-                    "No <a> element found inside the first div with class 'item-container'.")
-        else:
-            print("No div found with class 'item-container'.")
+        # Find the first <a> element inside the div
+        first_a_inside_div = first_item_container_div.find('a')
+
+        # Check if the <a> element was found
+        if not first_a_inside_div:
+            raise Exception("Item page link not found in first item")
+
+        # Extract the href attribute
+        final_url = first_a_inside_div.get('href')
 
         response = get_response(final_url)
 
@@ -155,10 +158,9 @@ def get_newegg_price(item: str) -> dict:
         else:
             raise Exception("Invalid Newegg product page response")
     else:
-        raise Exception("Invalid Walmart search page response")
+        raise Exception("Invalid Newegg search page response")
 
 
 # Main
 if __name__ == "__main__":
-    get_walmart_price(
-        'Sony XR85X93L 85" 4K Mini LED Smart Google TV with PS5 Features (2023)')
+    pass
